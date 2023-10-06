9c24c23ba86dc498846b843215c83f97f280c4d0
znataniel <nrojaszavalla@frba.utn.edu.ar>
2023-10-06 20:05:13 -0300

    Add computer play and one cycle of game functions

---
+++ js/game.js
@@ -0,0 +1,33 @@
+function getComputerPlay() {
+  let menu = Math.floor(Math.random * 3) + 1;
+  switch (menu) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+}
+
+function playGame(playerChoice, computerChoice) {
+
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (playerChoice == computerChoice) {
+    return "Tie! Both players picked " + playerChoice.toUpperCase();
+  }
+
+  if ((playerChoice == "rock" && computerChoice == "paper") || (playerChoice == "paper" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "rock")) {
+    return "Computer wins! " + computerChoice.toUpperCase() + " beats " + playerChoice.toUpperCase();
+  }
+
+  if ((playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "paper")) {
+    return "Player wins! " + playerChoice.toUpperCase + " beats " + computerChoice.toUpperCase();
+  }
+
+  return -1;
+
+}
+
